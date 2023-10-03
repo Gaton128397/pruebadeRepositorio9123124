73756ee9a16c81510ca0fd6185560e446293d3ff
Gaton128397 <gvillanueva19@alumnos.utalca.cl>
2023-10-02 22:07:49 -0300

    Cracion fallida matriz

--- clase.py
+++ clase.py
@@ -5,3 +5,13 @@ def prueba():
     print("hola")
 
 prueba()
+
+def rellenar_matriz(matriz, variable1, variable2):
+    for i in range(matriz):
+        for j in range(matriz):
+            matriz[i][j] = variable1 + variable2
+    return matriz
+matriz = []
+matriz = rellenar_matriz(matriz, 5, 2)
+
+print(matriz)
